f7aba16a44c218b8eb201c9b1255e67a9383f74e
tarnaldo88 <tarnaldo88@gmail.com>
2025-05-10 00:46:11 -0700

    file migration

---
+++ firebaseConfig.ts
@@ -0,0 +1,7 @@
+// Firebase initialization for React Native
+import  firebase  from '@react-native-firebase/app';
+
+// If you need to add custom initialization logic, do it here.
+// For most cases, just importing @react-native-firebase/app is enough.
+
+export default firebase;
