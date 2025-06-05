0e1880e97bfd4b9d0daad0a45a31b653bf9e3f6c
tarnaldo88 <tarnaldo88@gmail.com>
2025-06-05 14:13:38 -0700

    moved profile screen

---
+++ screens/ProfileScreen.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+function ProfileScreen(){
+  let te = "Placeholder";
+
+  return(
+    <View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    //bottom: -90,
+   // left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
+
+
+export default ProfileScreen;
