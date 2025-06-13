d193c2d9955be6e182480c2afa421e4a2cd58438
tarnaldo88 <tarnaldo88@gmail.com>
2025-06-13 14:32:25 -0700

    adding BG to goals

--- screens/GoalScreen.tsx
+++ screens/GoalScreen.tsx
@@ -1,7 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '../context/AuthContext';
+import goalBg from '../img/meditationBG.jpg';
 
 type RootStackParamList = {
     Goals: undefined;
@@ -10,7 +12,8 @@ type RootStackParamList = {
 };
 
 type GoalScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Goals'>;
-import { useAuth } from '../context/AuthContext';
+
+
 
 export default function GoalScreen() {
     const { user } = useAuth();
@@ -18,15 +21,17 @@ export default function GoalScreen() {
     const navigation = useNavigation<GoalScreenNavigationProp>();
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Goals</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('GoalsList', { userId })}>
-                <Text style={styles.buttonText}>View All Goals</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateGoal', { userId })}>
-                <Text style={styles.buttonText}>Create New Goal</Text>
-            </TouchableOpacity>
-        </View>
+        <ImageBackground source={goalBg} style={styles.background} resizeMode="cover">
+            <View style={styles.container}>
+                <Text style={styles.title}>Goals</Text>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('GoalsList', { userId })}>
+                    <Text style={styles.buttonText}>View All Goals</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CreateGoal', { userId })}>
+                    <Text style={styles.buttonText}>Create New Goal</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     );
 }
 
@@ -35,4 +40,12 @@ const styles = StyleSheet.create({
     title: { fontSize: 24, marginBottom: 20, color: 'gold' },
     button: { backgroundColor: '#c4e86c', padding: 10, marginVertical: 5, borderRadius: 5 },
     buttonText: { color: '#222d01', fontSize: 18 },
+
+    background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
