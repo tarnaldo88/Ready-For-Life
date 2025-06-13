a5685d0315545036ea41d5e98320db9e3f73ce17
tarnaldo88 <tarnaldo88@gmail.com>
2025-06-13 15:30:02 -0700

    Update GoalScreen.tsx

--- screens/GoalScreen.tsx
+++ screens/GoalScreen.tsx
@@ -37,7 +37,12 @@ export default function GoalScreen() {
 
 const styles = StyleSheet.create({
     container: { flex: 1, justifyContent: 'center', alignItems: 'center',  },
-    title: { fontSize: 24, marginBottom: 20, color: 'gold' },
+    title: { 
+        fontSize: 24, 
+        marginBottom: 20, 
+        fontWeight: 'bold',
+        color: 'gold', 
+    },
     button: { backgroundColor: '#c4e86c', padding: 10, marginVertical: 5, borderRadius: 5 },
     buttonText: { color: '#222d01', fontSize: 18 },
 
