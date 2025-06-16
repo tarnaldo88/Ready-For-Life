af0eeca246f71a6ff00ae478acaa3587adc2cc50
tarnaldo88 <tarnaldo88@gmail.com>
2025-06-16 01:08:31 -0700

    Update NutitrionScreen.tsx

--- screens/NutitrionScreen.tsx
+++ screens/NutitrionScreen.tsx
@@ -204,6 +204,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginTop: 8,
     justifyContent: 'flex-end',
+    marginRight: 8,
   },
   editButton: {
     backgroundColor: '#6cc6e8',
