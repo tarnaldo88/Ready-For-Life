ebf182c1abbc3f9d95dcaf89823d76397f7488ff
tarnaldo88 <tarnaldo88@gmail.com>
2025-08-05 02:03:08 -0700

    Update EditProfileScreen.tsx

--- screens/EditProfileScreen.tsx
+++ screens/EditProfileScreen.tsx
@@ -81,7 +81,7 @@ const EditProfileScreen: React.FC<EditProfileScreenProps> = () => {
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.buttonReturn}
-          onPress={() => navigation.navigate('Home', { userId})}
+          onPress={() => navigation.navigate('Home', {userId})}
           disabled={loading}
         >
           <Text style={styles.btnReturnText}> Return Home </Text>
